386243e1c8c9fe8fd7655c4ae7ed818e89fb5fa6
Kelsey Martens <kelsey@solspace.com>
2012-10-11 19:49:30 -0500

    Initial commit

---
+++ language/english/lang.shortcut.php
@@ -0,0 +1,211 @@
+<?php
+
+ /**
+ * Solspace - Shortcut
+ *
+ * @package		Solspace:Shortcut
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2010-2012, Solspace, Inc.
+ * @link		http://www.solspace.com/docs/addon/c/Shortcut/
+ * @version		2.0.0
+ * @filesource 	./system/expressionengine/third_party/shortcut/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"shortcut_module_name" =>
+"Shortcut",
+
+"shortcut_module_description" =>
+"Creates short URL aliases to access your URLs",
+
+"shortcut_module_version" =>
+"Shortcut",
+
+"modules" =>
+"Modules",
+
+//----------------------------------------
+//  Update
+//----------------------------------------
+
+'update' =>
+"Update",
+
+'update_shortcut' =>
+"Update the Shortcut Module",
+
+'update_shortcut_module' =>
+"Update the Shortcut Module",
+
+'update_failure' =>
+"The update was not successful.",
+
+'update_successful' =>
+"The update was successful",
+
+'shortcut_update_message' =>
+"It looks like you have uploaded a new version of Shortcut. Please run the upgrade script by clicking 'Update' below.",
+
+//----------------------------------------
+//  Preferences
+//----------------------------------------
+
+'preferences'	=>
+'Preferences',
+
+'shortcut_preferences_exp'	=>
+'Control your Shortcut module preferences using this form.',
+
+'shortcut_prefix'	=>
+'Shortcut Prefix',
+
+'shortcut_prefix_exp'	=>
+'The Shortcut Prefix is used to add a prefix to your shortcut urls.',
+
+'prefix_required'	=>
+'A prefix is required. Using a prefix allows the Shortcut module to recognize shortcut urls. Without the prefix, every single page request would have to first go through the Shortcut module. This would result in poor performance for your website.',
+
+'shortcut_ambiguous_chars' =>
+'Avoid using ambiguous characters in shortcut',
+
+'shortcut_ambiguous_chars_exp' =>
+'Do not use ambiguous characters in automatically generated URL\'s (1, i, l, 0, o)',
+
+//----------------------------------------
+//  Shortcuts
+//----------------------------------------
+
+'shortcut'	=>
+'Shortcut',
+
+'shortcuts'	=>
+'Shortcuts',
+
+'url'	=>
+'URL',
+
+'custom_shortcut'	=>
+'Custom Shortcut',
+
+'actions'	=>
+'Actions',
+
+'shortcuts_exp'	=>
+'Create shortcut urls here.',
+
+'shortcut_base'	=>
+'Shortcut Base URL',
+
+'shortcut_base_exp'	=>
+'You can override the site default base url with this URL.',
+
+'shortcut_exists'	=>
+'That shortcut already exists. Please try another.',
+
+'shortcut_invalid'	=>
+'That shortcut you provided is invalid. Please try again.',
+
+'shortcut_missing_prefix'	=>
+'Every shortcut must have your designated prefix. The prefix helps prevent poor page load performance. Your prefix is: %prefix%',
+
+'url_invalid'	=>
+'The URL you provided is invalid. Please try again.',
+
+'shortcut_saved'	=>
+'%shortcut% has been saved as your shortcut.',
+
+'confirm_delete'	=>
+'Are you sure you want to delete the shortcut: ',
+
+'shortcut_deleted'	=>
+'Your shortcut as been deleted.',
+
+'is'	=>
+'is',
+
+'hits'	=>
+'Hits',
+
+'enter_url'	=>
+'Enter URL',
+
+'shortened_url_for'	=>
+'Shortened URL for',
+
+'edit_this'	=>
+'Edit the shortcut, %shortcut_msg%, using the fields below.',
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'homepage' =>
+"Homepage",
+
+'online_documentation' =>
+"Online Documentation",
+
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+"Save",
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'invalid_request' =>
+"Invalid Request",
+
+'shortcut_module_disabled' =>
+"The Shortcut module is currently disabled.  Please insure it is installed and up to date by going 
+to the module's control panel in the ExpressionEngine Control Panel",
+
+'disable_module_to_disable_extension' =>
+"To disable this extension, you must disable its corresponding <a href='%url%'>module</a>.",
+
+'enable_module_to_enable_extension' =>
+"To enable this extension, you must install its corresponding <a href='%url%'>module</a>.",
+
+'cp_jquery_requred' =>
+"The 'jQuery for the Control Panel' extension must be <a href='%extensions_url%'>enabled</a> to use this module.",
+
+'incorrect_shortcut_prefix' =>
+"Incorrect shortcut prefix",
+
+//	----------------------------------------
+//	 Accessory
+//	----------------------------------------
+
+'create_new_shortcut' =>
+'Create a shortcut URL',
+
+'paste_any_url' =>
+'Enter original URL',
+
+'custom_shortcut_optional' =>
+'Custom shortcut (optional)',
+
+'get_shortcut' =>
+'Get Shortcut URL',
+
+'your_shortcut_url' =>
+'Your shortcut URL: ',
+
+'recent_shortcuts' =>
+'Recent Shortcuts',
+
+'no_shortcuts' =>
+'No shortcuts found. Make some now!',
+
+/* END */
+''=>''
+);
+?>
